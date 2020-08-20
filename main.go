de6eb10c866723c18ba2f62bcb15ca57e6839eec
Lucas Taniguti <lucas.biotec07@gmail.com>
2020-08-20 12:46:52 +0000

    skeleton of commands

--- main.go
+++ main.go
@@ -14,44 +14,29 @@ func main() {
 		Usage: "Command line interface for Cromwell Server",
 		Commands: []*cli.Command{
 			{
-				Name:    "add",
-				Aliases: []string{"a"},
-				Usage:   "add a task to the list",
+				Name:    "submit",
+				Aliases: []string{"s"},
+				Usage:   "Submit a new job for Cromwell Server",
+				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "wdl", Aliases: []string{"w"}, Required: true},
+					&cli.StringFlag{Name: "inputs", Aliases: []string{"i"}, Required: true},
+					&cli.StringFlag{Name: "dependencies", Aliases: []string{"d"}, Required: true},
+				},
 				Action: func(c *cli.Context) error {
 					fmt.Println("added task: ", c.Args().First())
 					return nil
 				},
 			},
 			{
-				Name:    "complete",
-				Aliases: []string{"c"},
-				Usage:   "complete a task on the list",
+				Name:    "kill",
+				Aliases: []string{"k"},
+				Usage:   "Kill a running job",
+				Flags: []cli.Flag{
+					&cli.StringFlag{Name: "operation", Aliases: []string{"o"}, Required: true},
+				},
 				Action: func(c *cli.Context) error {
 					fmt.Println("completed task: ", c.Args().First())
 					return nil
-				},
-			},
-			{
-				Name:    "template",
-				Aliases: []string{"t"},
-				Usage:   "options for task templates",
-				Subcommands: []*cli.Command{
-					{
-						Name:  "add",
-						Usage: "add a new template",
-						Action: func(c *cli.Context) error {
-							fmt.Println("new task template: ", c.Args().First())
-							return nil
-						},
-					},
-					{
-						Name:  "remove",
-						Usage: "remove an existing template",
-						Action: func(c *cli.Context) error {
-							fmt.Println("removed task template: ", c.Args().First())
-							return nil
-						},
-					},
 				},
 			},
 		},
