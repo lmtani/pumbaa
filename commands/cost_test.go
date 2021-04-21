f9e834e9637a5842028f0644b71804f8eda58958
Lucas Taniguti <lucas.biotec07@gmail.com>
2021-04-21 00:56:44 +0000

    cost command test

---
+++ commands/cost_test.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func ExampleResourcesUsed() {
+	// Read metadata mock
+	content, err := ioutil.ReadFile("../pkg/cromwell/mocks/metadata.json")
+	if err != nil {
+		fmt.Print("Coult no read metadata mock file metadata.json")
+	}
+
+	// Mock http server
+	operation := "aaaa-bbbb-uuid"
+	ts := buildTestServer("/api/workflows/v1/"+operation+"/metadata", string(content))
+	defer ts.Close()
+
+	err = ResourcesUsed(ts.URL, "", operation)
+	if err != nil {
+		log.Print(err)
+	}
+	// Output:
+	// +---------------+---------------+------------+---------+
+	// |   RESOURCE    | NORMALIZED TO | PREEMPTIVE | NORMAL  |
+	// +---------------+---------------+------------+---------+
+	// | CPUs          | 1 hour        | 1440.00    | 720.00  |
+	// | Memory (GB)   | 1 hour        | 2880.00    | 1440.00 |
+	// | HDD disk (GB) | 1 month       | -          | -       |
+	// | SSD disk (GB) | 1 month       | 40.00      | 20.00   |
+	// +---------------+---------------+------------+---------+
+}
