46d72d931400ec26433f0937e2c0cc5ab6c3021c
Lucas Taniguti <ltaniguti@gmail.com>
2022-01-06 19:55:34 -0300

    🧪 Return failing test

--- internal/commands/examples_test.go
+++ internal/commands/examples_test.go
@@ -268,3 +268,60 @@ func ExampleCommands_Wait() {
 	// Status=Done
 	// Time between status check = 1
 }
+
+func ExampleCommands_MetadataWorkflow() {
+	// Read metadata mock
+	content, err := ioutil.ReadFile("../../pkg/cromwell/mocks/metadata.json")
+	if err != nil {
+		fmt.Print("Coult no read metadata mock file metadata.json")
+	}
+
+	// Mock http server
+	operation := "aaaa-bbbb-uuid"
+	ts := BuildTestServer("/api/workflows/v1/"+operation+"/metadata", string(content), http.StatusOK)
+	defer ts.Close()
+
+	cmds := BuildTestCommands(ts.URL, "", "", 0)
+	err = cmds.MetadataWorkflow(operation)
+	if err != nil {
+		log.Print(err)
+	}
+	// Output:
+	// +-------------------+---------+----------+--------+
+	// |       TASK        | ATTEMPT | ELAPSED  | STATUS |
+	// +-------------------+---------+----------+--------+
+	// | RunHelloWorkflows | 1       | 7.515s   | Done   |
+	// | RunHelloWorkflows | 1       | 7.514s   | Done   |
+	// | SayGoodbye        | 1       | 720h0m0s | Done   |
+	// | SayHello          | 1       | 720h0m0s | Done   |
+	// | SayHelloCache     | 1       | 720h0m0s | Done   |
+	// +-------------------+---------+----------+--------+
+}
+
+func ExampleCommands_MetadataWorkflow_second() {
+	// Read metadata mock
+	content, err := ioutil.ReadFile("../../pkg/cromwell/mocks/metadata-failed.json")
+	if err != nil {
+		fmt.Print("Coult no read metadata mock file metadata.json")
+	}
+
+	// Mock http server
+	operation := "aaaa-bbbb-uuid"
+	ts := BuildTestServer("/api/workflows/v1/"+operation+"/metadata", string(content), http.StatusOK)
+	defer ts.Close()
+
+	cmds := BuildTestCommands(ts.URL, "", "", 0)
+	err = cmds.MetadataWorkflow(operation)
+	if err != nil {
+		log.Print(err)
+	}
+	// Output:
+	// +------+---------+---------+--------+
+	// | TASK | ATTEMPT | ELAPSED | STATUS |
+	// +------+---------+---------+--------+
+	// +------+---------+---------+--------+
+	// ❗You have 1 issue:
+	//
+	//  - Workflow input processing failed
+	//  - Required workflow input 'HelloWorld.name' not specified
+}
