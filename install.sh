fdbd823f6b9bd081c3b06307bd157ce179bf5835
Lucas Taniguti <lucas.taniguti@mendelics.com.br>
2023-04-13 22:42:47 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -151,7 +151,7 @@ main() {
 
     prefix="${1}"
     cli_base_url="https://github.com/lmtani/cromwell-cli/releases/download"
-    version="0.10.0"
+    version="0.9.2"
     print_message "== Install prefix set to ${prefix}" "info"
 
     cli_arch="$(determine_arch)"
